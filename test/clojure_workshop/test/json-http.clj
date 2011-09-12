def0f9ca091c9ac5be18663008716c051d027e1e
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-09-12 22:10:57 +0200

    Parsing twitter json, seems simpler than github.

--- test/clojure_workshop/test/json-http.clj
+++ test/clojure_workshop/test/json-http.clj
@@ -1,12 +1,17 @@
 (ns clojure_workshop.test.json-http
   (:require [clojure.contrib.io :as io]
             [clojure.contrib.http.agent :as http]
-            [clojure.contrib.json :as json]))
+            [clojure.contrib.json :as json]
+            [clojure.walk :as walk]))
 
-(defn handle-json [jsn]
+(defn print-json [jsn]
   (json/pprint-json jsn))
 
+(defn twitter-text-result [{res :results}]
+  (print (interpose "\n" (map :text res))))
+
 (http/http-agent
-  "https://api.github.com/repos/clojure/clojure/commits"
-  :handler #(-> % http/stream io/reader json/read-json handle-json))
+  ;"https://api.github.com/repos/clojure/clojure/commits"
+  "http://search.twitter.com/search.json?q=java&result_type=mixed&count=5"
+  :handler #(-> % http/stream io/reader json/read-json twitter-text-result))
 
