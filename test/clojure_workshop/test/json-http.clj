c596439243f3817d8b98a849d47ae18f8485c6be
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-09-14 18:50:09 +0200

    Illustrating difference between -> let and normal function calls.

--- test/clojure_workshop/test/json-http.clj
+++ test/clojure_workshop/test/json-http.clj
@@ -2,16 +2,36 @@
   (:require [clojure.contrib.io :as io]
             [clojure.contrib.http.agent :as http]
             [clojure.contrib.json :as json]
-            [clojure.walk :as walk]))
+            [clojure.string]))
 
-(defn print-json [jsn]
+(defn- print-json [jsn]
   (json/pprint-json jsn))
 
-(defn twitter-text-result [{res :results}]
-  (print (interpose "\n" (map :text res))))
+(defn- twitter-text-result [{res :results}]
+  (println
+    (clojure.string/join "\n"
+      (map :text res))))
+
+;; Three following functions does the same, which do you prefer?
+(defn- agent-handler [agt]
+  (twitter-text-result
+    (json/read-json
+      (io/reader
+        (http/stream agt)))))
+
+(defn- agent-handler-> [agt]
+  (-> agt http/stream io/reader json/read-json twitter-text-result))
+
+(defn- agent-handler-let [agt]
+  (let [str (http/stream agt)
+        rdr (io/reader str)
+        jsn (json/read-json rdr)]
+    (twitter-text-result jsn)))
+
+(agent-handler
+  (http/http-agent "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10"))
 
 (http/http-agent
-  ;"https://api.github.com/repos/clojure/clojure/commits"
-  "http://search.twitter.com/search.json?q=java&result_type=mixed&count=5"
-  :handler #(-> % http/stream io/reader json/read-json twitter-text-result))
+  "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10"
+  :handler agent-handler)
 
