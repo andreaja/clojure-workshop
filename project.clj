3158701ee9394d3c0c1fe711bb00f6ff6d2a9c40
Andreas Jacobsen <andreaja@ifi.uio.no>
2011-09-21 16:46:32 +0300

    Lagt til swank-clojure avhengighet.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]])
+  :dev-dependencies [[lein-eclipse "1.0.0"]
+                     [swank-clojure "1.3.2"]])
