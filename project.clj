560d6f329f1c9ed1bdf4fce39b5d50002905edb3
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-09-25 16:55:50 +0200

    Updated to clojure 1.3. Had to use custom built version of clojure.contrib.io and clojure.http.agent. Build and deployed on nexus.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clojure-workshop "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/contrib.io "0.1.0"]
+                 [org.clojure/data.json "0.1.0"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]
-                     [swank-clojure "1.3.2"]])
+                     [swank-clojure "1.3.2"]]
+  :repositories {"knowit-patches" "http://projects.knowit.no/nexus/content/repositories/thirdparty"})
