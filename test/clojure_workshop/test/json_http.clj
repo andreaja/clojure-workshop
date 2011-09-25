61ba8773da3a5ebc1e0068db4a0c426ae2f21714
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-09-25 17:01:48 +0200

    Making example, don't need other metods.c

--- test/clojure_workshop/test/json_http.clj
+++ test/clojure_workshop/test/json_http.clj
@@ -1,7 +1,7 @@
 (ns clojure-workshop.test.json-http
   (:require [clojure.contrib.io :as io]
             [clojure.contrib.http.agent :as http]
-            [clojure.contrib.json :as json]
+            [clojure.data.json :as json]
             [clojure.string]))
 
 (defn- print-json [jsn]
@@ -12,25 +12,9 @@
     (clojure.string/join "\n"
       (map :text res))))
 
-;; Three following functions does the same, which do you prefer?
-(defn- agent-handler [agt]
-  (twitter-text-result
-    (json/read-json
-      (io/reader
-        (http/stream agt)))))
-
 (defn- agent-handler-> [agt]
   (-> agt http/stream io/reader json/read-json twitter-text-result))
 
-(defn- agent-handler-let [agt]
-  (let [str (http/stream agt)
-        rdr (io/reader str)
-        jsn (json/read-json rdr)]
-    (twitter-text-result jsn)))
-
-(agent-handler
+(agent-handler->
   (http/http-agent "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10"))
 
-(http/http-agent
-  "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10"
-  :handler agent-handler)
