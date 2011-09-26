812b84432facae490a8a85049b0f54a36b691ad1
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-09-26 15:36:44 +0200

    Removed solutions to existing tasks.

--- test/clojure_workshop/test/core.clj
+++ test/clojure_workshop/test/core.clj
@@ -3,40 +3,40 @@
   (:use [clojure.test]))
 
 (defn __ [& args] false)
-(def ___ '())
+(def ___ identity)
 
 (deftest very-basic-types
   (is (= 1 1))
-  (is (true? true))
-  (is (= (str \a \b \c) "abc"))
+  (is (true? __))
+  (is (= (str \a \b \c) __))
   ;; Bruk vector literal
-  (is (= [1 2 3] [1 2 3]))
+  (is (= [1 2 3] __))
   ;; Bruk list literal
-  (is (= [1 2 3] '(1 2 3)))
+  (is (= [1 2 3] __))
   ;; Bruk list function
-  (is (= [1 2 3] (list 1 2 3)))
+  (is (= [1 2 3] __))
   ;; Bruk vec function
-  (is (= [1 2 3] (vector 1 2 3))))
+  (is (= [1 2 3] __)))
 
 (deftest use-conjoin
   (are [x y] (= x y)
-    (conj [1] 2) [1 2]
-    (conj [1 2] 3 4) [1 2 3 4]
-    (conj '(2 1) 3 4) '(4 3 2 1)
-    (conj #{2 1} 3 4) #{1 2 3 4}))
+    (conj [1] 2) __
+    (conj [1 2] 3 4) __
+    (conj '(2 1) 3 4) __
+    (conj #{2 1} 3 4) __))
 
 ;; Bruk (doc conj) for info
 
 (deftest hvordan-telle-ting
   (are [x y] (= x y)
-       5 (count '(1 2 3 4 5))
-       4 (count [1 2 3 4])
-       10 (count (range 10))
-       4 (count {:a 1, :b 2, :c 3, :d 4})
-       8 (count "En banan")))
+       5 (___ '(1 2 3 4 5))
+       4 (___ [1 2 3 4])
+       10 (___ (range 10))
+       4 (___ {:a 1, :b 2, :c 3, :d 4})
+       8 (___ "En banan")))
 
 (deftest how-to-find-length-of-something
-  (let [f count]
+  (let [f ___]
     (are [x y] (= x y)
       (f '(1 2 3 3 1)) 5
       (f "Hello World") 11
@@ -47,7 +47,7 @@
 
 (deftest hvordan-filtrere-ut-det-du-vil-ha
   (are [x y] (= x y)
-       (filter odd? '(1 2 3 4 5)) '(1 3 5)
-       (filter even? '(1 2 3 4 5)) '(2 4)))
+       (filter odd? '(1 2 3 4 5)) __
+       (filter even? '(1 2 3 4 5)) __))
 
 (run-tests)
