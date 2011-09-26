7ed4fba36cf71d21661b747b1fc2581ac4e602cf
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-09-26 16:53:46 +0200

    More with twitter json.

--- test/clojure_workshop/test/json_http.clj
+++ test/clojure_workshop/test/json_http.clj
@@ -1,20 +1,35 @@
 (ns clojure-workshop.test.json-http
-  (:require [clojure.contrib.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.contrib.http.agent :as http]
             [clojure.data.json :as json]
             [clojure.string]))
 
-(defn- print-json [jsn]
+(defn print-json [jsn]
   (json/pprint-json jsn))
 
-(defn- twitter-text-result [{res :results}]
-  (println
-    (clojure.string/join "\n"
-      (map :text res))))
 
-(defn- agent-handler-> [agt]
-  (-> agt http/stream io/reader json/read-json twitter-text-result))
+(defn- agent-handler [agt]
+  (-> agt http/stream io/reader json/read-json))
 
-(agent-handler->
-  (http/http-agent "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10"))
+(defn print-fn-result [f]
+  (println (f (agent-handler
+                (http/http-agent "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10")))))
 
+#_
+(defn slurp-file-json []
+  (json/read-json
+    (slurp "twitter-java-search-json.txt")))
+
+;; Hint hent results og deretter text. For fin formattering sjekk ut clojure/string.
+(defn text-from-twitter-search [search-result]
+  (clojure.string/join "\n"
+    (map :text (:results search-result))))
+
+(defn unique-set-of-tweet-encodings [search-result]
+  (set (map :iso_language_code (:results search-result))))
+
+
+(print-fn-result text-from-twitter-search)
+(print-fn-result unique-set-of-tweet-encodings)
+
+;(println (slurp-file-json))
