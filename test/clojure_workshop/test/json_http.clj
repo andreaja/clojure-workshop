4d7158b0e11f0eddd634e47ca9a482faa811798f
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-10-02 10:38:35 +0200

    Some function comments.

--- test/clojure_workshop/test/json_http.clj
+++ test/clojure_workshop/test/json_http.clj
@@ -6,25 +6,33 @@
 
 (def twitter-search-url "http://search.twitter.com/search.json?q=usa&result_type=recent&count=10")
 
-(defn print-json [jsn]
-  (json/pprint-json jsn))
+(defn print-json
+  "Prints json in a 'pretty' way to *out*"
+  [jsn] (json/pprint-json jsn))
 
-(defn search-twitter []
-  (-> twitter-search-url client/get :body json/read-json))
+(defn search-twitter
+  "Search twitter using 'twitter-search-url'.
+   Converts it to internal json representation before returning it."
+  [] (-> twitter-search-url client/get :body json/read-json))
 
-(defn print-fn-result [f json-result-fn]
+(defn print-fn-result
+  "Prints results of a function call 'f' and returns the result"
+  [f json-result-fn]
   (let [res (f (json-result-fn))]
     (println res)
     res))
 
-(defn slurp-file-json []
-  (json/read-json
+(defn slurp-file-json
+  "Reads 'twitter-java-search-json.txt' from the file system and converts it to json"
+  [] (json/read-json
     (slurp "twitter-java-search-json.txt")))
+
 
 ;; Hint hent results og deretter text. For fin formattering sjekk ut clojure/string.
 (defn text-from-twitter-search [search-result]
   (clojure.string/join "\n"
     (map :text (:results search-result))))
+
 
 ;; Hint hent results og deretter text, mappe så innhold av collection til kun å være encoding (iso_language_code).
 (defn unique-set-of-tweet-encodings [search-result]
@@ -43,3 +51,5 @@
 
 ; Skriv ut tweets fra som svarer/sender til noen
 (println (map :text (filter #(not= nil (:to_user %)) (:results (slurp-file-json )))))
+
+;; Har du fremdeles mer tid? Tenk på hvordan du kan gjøre http kallene asynkrone.
