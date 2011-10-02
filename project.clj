5a1726c125fb7adeaeeca24b46748084d50d812b
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-10-02 11:42:25 +0200

    Using latest json library.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.2.1"]
-                 [org.clojure/data.json "0.1.0"]]
+                 [org.clojure/data.json "0.1.1"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]
                      [swank-clojure "1.3.2"]])
