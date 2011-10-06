69061402126900cdf175d37fbdc5e546ed41b4cc
Andreas Jacobsen <andreaja@ifi.uio.no>
2011-10-06 19:14:12 +0200

    Better name for test.

--- test/clojure_workshop/test/core.clj
+++ test/clojure_workshop/test/core.clj
@@ -95,7 +95,7 @@
          )))
 
 
-(deftest use-map-to-manipulate-all-elements-in-a-sequence
+(deftest use-map-to-double-all-numbers-in-a-sequence
   (let [double __]
     (are [x y] (= x y)
         (map double '(1 2 3)) '(2 4 6)
